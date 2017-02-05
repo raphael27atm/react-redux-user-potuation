b5eec002a1371e0e19ca94e9a2249e25542c89bb
Marlon Raphael <raphael27atm@gmail.com>
2017-02-05 16:23:51 -0300

    Create componet Stopwatch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Scoreboard from './containers/Scoreboard';
+
+import './App.css'
 
 ReactDOM.render(
-  <App />,
+  <Scoreboard />,
   document.getElementById('root')
 );
